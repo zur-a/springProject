5a367336ca48bedb63a9ff1fae41ff7369d7c7d8
Zura <jur.and.jr42@gmail.com>
2023-09-27 15:10:23 -0300

    Moved controller to BookController class

--- src/main/java/com/joi/springProject/SpringProjectApplication.java
+++ src/main/java/com/joi/springProject/SpringProjectApplication.java
@@ -1,34 +1,15 @@
 package com.joi.springProject;
 
-import controller.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Book> list() {
-		return List.of(
-				new Book(
-						1L,
-						"A divina Comédia",
-						"Dante Alighieri",
-						"978-85-7326-121-9",
-						"Editora 34",
-						LocalDate.of(2020, Month.NOVEMBER, 4))
-		);
-	}
+
 
 }
--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,5 +1,25 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
 public class BookController {
-    
+    @GetMapping
+    public List<Book> list() {
+        return List.of(
+                new Book(
+                        1L,
+                        "A divina Comédia",
+                        "Dante Alighieri",
+                        "978-85-7326-121-9",
+                        "Editora 34",
+                        LocalDate.of(2020, Month.NOVEMBER, 4))
+        );
+    }
+
 }
