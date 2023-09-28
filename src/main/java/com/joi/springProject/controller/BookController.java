c1409c1c38acf345e41225029dcea3460f8bbd40
Zura <jur.and.jr42@gmail.com>
2023-09-28 14:03:51 -0300

    Moved getBooks implementation to the service layer

--- src/main/java/com/joi/springProject/controller/BookController.java
+++ src/main/java/com/joi/springProject/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.joi.springProject.controller;
 
+import com.joi.springProject.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -9,16 +11,12 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/book")
 public class BookController {
+
+    @Autowired
+    BookService service;
+
     @GetMapping()
     public List<Book> getBooks() {
-        return List.of(
-                new Book(
-                        1L,
-                        "A divina Comédia",
-                        "Dante Alighieri",
-                        "978-85-7326-121-9",
-                        "Editora 34",
-                        LocalDate.of(2020, Month.NOVEMBER, 4))
-        );
+        return service.getBooks();
     }
 }
--- src/main/java/com/joi/springProject/service/BookService.java
+++ src/main/java/com/joi/springProject/service/BookService.java
@@ -1,5 +1,21 @@
 package com.joi.springProject.service;
 
+import com.joi.springProject.controller.Book;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 public class BookService {
-    
+    public List<Book> getBooks() {
+        return List.of(
+                new Book(
+                        1L,
+                        "A divina Comédia",
+                        "Dante Alighieri",
+                        "978-85-7326-121-9",
+                        "Editora 34",
+                        LocalDate.of(2020, Month.NOVEMBER, 4))
+        );
+    }
 }
